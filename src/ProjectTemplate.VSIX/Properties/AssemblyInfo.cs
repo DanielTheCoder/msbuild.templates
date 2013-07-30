4fb69b34c6f35cd7e9134315ec8835b775077157
Daniel Sack <daniel@sack.at>
2013-07-30 16:04:43 +0200

    Project Template + VSIX generator

--- src/ProjectTemplate.VSIX/Properties/AssemblyInfo.cs
+++ src/ProjectTemplate.VSIX/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MSBuild.ProjectTemplate")]
+[assembly: AssemblyTitle("MSBuild.ProjectTemplate.VSIX")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MSBuild.ProjectTemplate")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyProduct("MSBuild.ProjectTemplate.VSIX")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -18,9 +18,6 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c908e465-6dca-46fd-9326-f73bd58b1ee4")]
 
 // Version information for an assembly consists of the following four values:
 //
