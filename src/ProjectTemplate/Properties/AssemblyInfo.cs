296161dee280823afde9e2cf84b07950a7928e8f
Daniel Sack <daniel@sack.at>
2013-08-25 10:11:57 +0200

    Item Templates added

--- src/ProjectTemplate/Properties/AssemblyInfo.cs
+++ src/ProjectTemplate/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("MSBuild.ProjectTemplate")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Daniel Sack")]
 [assembly: AssemblyProduct("MSBuild.ProjectTemplate")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
